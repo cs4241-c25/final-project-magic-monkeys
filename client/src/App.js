6b112a9076e5d824754c87227e2dfd6ef952fde6
FrancescoDM6 <112992086+FrancescoDM6@users.noreply.github.com>
2025-02-15 17:18:47 -0500

    Auth0 setup with example env file. Updated react and react dom versions

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
 
 function App() {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to MovieMates</h1>
+
+          {!isAuthenticated ? (
+              <button onClick={() => loginWithRedirect()}>Log In</button>
+          ) : (
+              <>
+                <p>Welcome, {user?.name}!</p>
+                <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                  Log Out
+                </button>
+              </>
+          )}
+        </header>
+      </div>
   );
 }
 
