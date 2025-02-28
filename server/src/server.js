3c88f13760fa84fa61c54a085c1b066c38f27d4c
FatalCodes <skyematty@gmail.com>
2025-02-28 10:45:50 -0500

    Build script change

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,11 @@
 const app = require('./app');
 const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 // Connect to MongoDB
 connectDB().then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
   });
 }).catch((err) => {
