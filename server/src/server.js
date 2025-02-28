95151a8b387e82034d53636c36b91cf505171d23
FatalCodes <skyematty@gmail.com>
2025-02-28 10:50:07 -0500

    Deleted connection

--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,11 @@ const connectDB = require('./config/db');
 const PORT = process.env.PORT || 3001;
 
 // Connect to MongoDB
-connectDB().then(() => {
-  app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}).catch((err) => {
-  console.error('Failed to connect to DB', err);
-  process.exit(1);
-});
+// connectDB().then(() => {
+//   app.listen(PORT, '0.0.0.0', () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// }).catch((err) => {
+//   console.error('Failed to connect to DB', err);
+//   process.exit(1);
+// });
