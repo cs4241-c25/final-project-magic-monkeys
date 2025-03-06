f587cc073d38b6487b4757ba08ae86a2719f80aa
Parker Glispin <pbglispin@wpi.edu>
2025-03-06 18:23:10 -0500

    new review route

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,19 +1,13 @@
 import express from "express";
-import {
-    createReview,
-    getReviewById,
-    getReviewsByUser,
-    updateReview,
-    deleteReview,
-    getGroupMovieAverageRating
-} from "../controllers/reviewController.js";
+import { createReview, getReviewById, getReviewsByUser, getReviewByUserMovie, getGroupAverageMovieReview, updateReview, deleteReview } from "../controllers/reviewController.js";
 
 const router = express.Router();
 
 router.post("/reviews", createReview);
 router.get("/reviews/:id", getReviewById);
 router.get("/users/:userId/reviews", getReviewsByUser);
-router.get("/groups/:groupId/movies/:movieId/average-rating", getGroupMovieAverageRating);
+router.get("/users/:userId/movie/:movieId", getReviewByUserMovie)
+router.get("/groups/:groupId/movies/:movieId/average-rating", getGroupAverageMovieReview);
 router.put("/reviews/:id", updateReview);
 router.delete("/reviews/:id", deleteReview);
 
