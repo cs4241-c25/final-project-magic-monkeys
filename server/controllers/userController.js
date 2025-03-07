3cbd1904d89bc96f10eee22189acd7ba845da11f
Parker Glispin <pbglispin@wpi.edu>
2025-03-07 01:19:34 -0500

    fixed route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -29,7 +29,7 @@ export const createUser = async (req, res) => {
 
         const newUser = new User({
             auth0Id,
-            uniqueUsername,
+            username: uniqueUsername,
             email,
             profilePicture,
             favoriteMovie,
